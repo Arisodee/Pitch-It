7497e07bcc6d7817ceddc59d3dfedda71a2363ed
Arisodee <arisodee@gmail.com>
2020-12-06 22:33:57 +0300

    Change application structure and configurations

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,16 @@ from .config import DevConfig
 
 bootstrap = Bootstrap()
 
-# Initializing application
-app = Flask(__name__)
+def create_app(config_name):
 
-# Setting up configuration
-app.config.from_object(DevConfig)
+    app = Flask(__name__)
 
-from app import views
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
