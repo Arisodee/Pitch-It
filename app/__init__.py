c3e1c51e612c96a3ec8c22cef69f7ff1b8fea180
Arisodee <arisodee@gmail.com>
2020-12-06 14:03:25 +0300

    Add Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+
+bootstrap = Bootstrap()
 
 def create_app(config_name):
 
-    app = Flask(__name__
+    app = Flask(__name__)
+
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+
+    return app
