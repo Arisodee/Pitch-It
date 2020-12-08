ee37b25c87034f66afd847417abf72222c1cd895
Arisodee <arisodee@gmail.com>
2020-12-09 01:47:03 +0300

    Fix errors

--- app/main/forms.py
+++ app/main/forms.py
@@ -11,7 +11,6 @@ class CommentForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class PitchForm(FlaskForm):
-
     title = StringField('Pitch title',validators=[Required()])
     text = TextAreaField('Text',validators=[Required()])
     category = SelectField('Type',choices=[('interview','Interview pitch'),('product','Product pitch'),('promotion','Promotion pitch')],validators=[Required()])
