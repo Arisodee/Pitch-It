42240b65e946b72a994cab2b7e6e52d75b9ed6ef
Arisodee <arisodee@gmail.com>
2020-12-08 10:38:33 +0300

    Convert markdown to html

--- config.py
+++ config.py
@@ -4,8 +4,22 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    SECRET_KEY = 'ariso'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ariso:Barbie1991@localhost/pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 
 class ProdConfig(Config):
@@ -25,6 +39,8 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ariso:Barbie1991@localhost/pitches'
 
     DEBUG = True
 
